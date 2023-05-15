a5633822f3dc3f8eef7f9e51b985c385b25aaabc
Константин Петрович <P3tr0viCh@users.noreply.github.com>
2023-05-15 09:40:25 +0500

    +Settings

--- P3tr0viCh.Utils/Properties/AssemblyInfo.cs
+++ P3tr0viCh.Utils/Properties/AssemblyInfo.cs
@@ -6,12 +6,17 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("P3tr0viCh.Utils")]
 [assembly: AssemblyDescription("Общие функции")]
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("П3тр0виЧъ")]
 [assembly: AssemblyProduct("P3tr0viCh.Utils")]
 [assembly: AssemblyCopyright("© П3тр0виЧъ")]
 [assembly: AssemblyTrademark("П3тр0виЧъ™")]
 [assembly: AssemblyCulture("")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -31,5 +36,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.7.*")]
+[assembly: AssemblyFileVersion("0.7.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
