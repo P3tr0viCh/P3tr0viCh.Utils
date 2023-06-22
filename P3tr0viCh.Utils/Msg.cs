9abb69954503d071d40920b6f04004a226210197
Константин Петрович <P3tr0viCh@users.noreply.github.com>
2023-06-22 09:05:29 +0500

    Update Msg.cs

--- P3tr0viCh.Utils/Msg.cs
+++ P3tr0viCh.Utils/Msg.cs
@@ -8,23 +8,43 @@ namespace P3tr0viCh.Utils
         {
             MessageBox.Show(text, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
+        
+        public static void Info(string format, object arg0)
+        {
+            Info(string.Format(format, arg0));
+        }
+        
+        public static void Info(string format, object arg0, object arg1)
+        {
+            Info(string.Format(format, arg0, arg1));
+        }
 
         public static bool Question(string text = "To be or not to be?")
         {
             return MessageBox.Show(text, Application.ProductName, MessageBoxButtons.YesNo, MessageBoxIcon.Question) ==
                 DialogResult.Yes;
         }
+       
+        public static void Question(string format, object arg0)
+        {
+            Question(string.Format(format, arg0));
+        }
+        
+        public static void Question(string format, object arg0, object arg1)
+        {
+            Question(string.Format(format, arg0, arg1));
+        }
 
         public static void Error(string text = "Error!")
         {
             MessageBox.Show(text, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
-
+        
         public static void Error(string format, object arg0)
         {
             Error(string.Format(format, arg0));
         }
-
+        
         public static void Error(string format, object arg0, object arg1)
         {
             Error(string.Format(format, arg0, arg1));
