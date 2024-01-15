e86e532031e2dcda31bcb04eacdae6851a744016
Константин Петрович <P3tr0viCh@users.noreply.github.com>
2024-01-15 14:58:19 +0500

    + Extensions.

---
+++ P3tr0viCh.Utils/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Drawing;
+
+namespace P3tr0viCh.Utils
+{
+    public static class ColorConverterExtensions
+    {
+        public static string ToHexString(this Color c) => $"#{c.R:X2}{c.G:X2}{c.B:X2}";
+
+        public static string ToRgbString(this Color c) => $"RGB({c.R}, {c.G}, {c.B})";
+    }
+
+    public static class TimeSpanConverterExtensions
+    {
+        public static string ToHoursMinutesString(this TimeSpan timeSpan) =>
+            $"{((timeSpan.TotalHours > 23) ? (int)timeSpan.TotalHours : timeSpan.Hours):D2}:{timeSpan.Minutes:D2}";
+    }
+}
--- P3tr0viCh.Utils/Properties/AssemblyInfo.cs
+++ P3tr0viCh.Utils/Properties/AssemblyInfo.cs
@@ -19,6 +19,6 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("bef314a3-4027-489b-8402-85c22a780465")]
 
-[assembly: AssemblyVersion("1.2.*")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.3.*")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
 [assembly: AssemblyInformationalVersion("1.0.0.0")]
