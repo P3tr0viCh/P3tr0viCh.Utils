a6900491b21a3d1c7ed9cbbc74bf10fa2beb7fe8
Константин Петрович <P3tr0viCh@users.noreply.github.com>
2024-06-25 10:43:13 +0500

    + ImageBtn.

---
+++ P3tr0viCh.Utils/ImageBtn.cs
@@ -0,0 +1,88 @@
+﻿using System.Windows.Forms;
+using System;
+
+namespace P3tr0viCh.Utils
+{
+    public class ImageBtn
+    {
+        public PictureBox Button { get; private set; }
+
+        private int imageNormal;
+        public int ImageNormal
+        {
+            get
+            {
+                return imageNormal;
+            }
+            set
+            {
+                imageNormal = value;
+
+                UpdateImage(!Button.MouseIsOverControl());
+            }
+        }
+        private int imageHover;
+        public int ImageHover
+        {
+            get
+            {
+                return imageHover;
+            }
+            set
+            {
+                imageHover = value;
+
+                UpdateImage(!Button.MouseIsOverControl());
+            }
+        }
+
+        public void SetImages(int imageNormal, int imageHover)
+        {
+            this.imageNormal = imageNormal;
+            this.imageHover = imageHover;
+
+            UpdateImage(!Button.MouseIsOverControl());
+        }
+
+        private ImageList imageList;
+        public ImageList ImageList
+        {
+            get { return imageList; }
+            set
+            {
+                imageList = value;
+
+                UpdateImage(!Button.MouseIsOverControl());
+            }
+        }
+
+        public ImageBtn(PictureBox button, ImageList imageList, int imageNormal, int imageHover)
+        {
+            Button = button;
+
+            this.imageList = imageList;
+
+            SetImages(imageNormal, imageHover);
+
+            UpdateImage(!Button.MouseIsOverControl());
+
+            Button.MouseEnter += Button_MouseEnter;
+            Button.MouseLeave += Button_MouseLeave;
+        }
+
+        private void UpdateImage(bool normalState)
+        {
+            Button.Image = ImageList.Images[normalState ? ImageNormal : ImageHover];
+        }
+
+        private void Button_MouseLeave(object sender, EventArgs e)
+        {
+            UpdateImage(true);
+        }
+
+        private void Button_MouseEnter(object sender, EventArgs e)
+        {
+            UpdateImage(false);
+        }
+    }
+}
