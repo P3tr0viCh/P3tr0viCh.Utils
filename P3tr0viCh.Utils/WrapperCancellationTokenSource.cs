5d7f750df8051098187e220374e7668109fc44a6
Константин Петрович <P3tr0viCh@users.noreply.github.com>
2024-09-20 08:57:06 +0500

    + WrapperCancellationTokenSource.

---
+++ P3tr0viCh.Utils/WrapperCancellationTokenSource.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading;
+
+namespace P3tr0viCh.Utils
+{
+    public class WrapperCancellationTokenSource
+    {
+        private CancellationTokenSource cancellationTokenSource;
+
+        public void Finally()
+        {
+            cancellationTokenSource.Dispose();
+            cancellationTokenSource = null;
+        }
+
+        public void Cancel()
+        {
+            cancellationTokenSource?.Cancel();
+            cancellationTokenSource?.Dispose();
+        }
+
+        public void Start()
+        {
+            Cancel();
+
+            cancellationTokenSource = new CancellationTokenSource();
+        }
+
+        public CancellationToken Token => cancellationTokenSource.Token;
+
+        public bool IsCancellationRequested => cancellationTokenSource.IsCancellationRequested;
+    }
+}
