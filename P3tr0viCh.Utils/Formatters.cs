6b95e395fc2e926e2aafd25669964c1426679c57
Константин Петрович <P3tr0viCh@users.noreply.github.com>
2025-03-11 11:44:32 +0500

    + Formatters.

---
+++ P3tr0viCh.Utils/Formatters.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace P3tr0viCh.Utils
+{
+    public static class Formatters
+    {
+        public class BoolFormatter : ICustomFormatter, IFormatProvider
+        {
+            public object GetFormat(Type formatType)
+            {
+                if (formatType == typeof(ICustomFormatter))
+                {
+                    return this;
+                }
+
+                return null;
+            }
+
+            public string Format(string format, object arg, IFormatProvider formatProvider)
+            {
+                if (arg is bool value)
+                {
+                    if (format.IsEmpty())
+                    {
+                        return value.ToString();
+                    }
+
+                    var words = format.Split('|');
+
+                    return value ? words[0] : (words.Length > 1 ? words[1] : string.Empty);
+                }
+
+                return string.Empty;
+            }
+        }
+    }
+}
