055e3ad0565db8f4e099019a2423ac0d5cd8dfaa
Константин Петрович <P3tr0viCh@users.noreply.github.com>
2025-11-20 12:34:04 +0500

    + PresenterStatusStrip.

---
+++ P3tr0viCh.Utils/PresenterStatusStrip.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace P3tr0viCh.Utils
+{
+    public class PresenterStatusStrip<T> where T : Enum
+    {
+        public interface IPresenterStatusStrip
+        {
+            ToolStripStatusLabel GetLabel(T label);
+        }
+
+        public IPresenterStatusStrip View { get; private set; }
+
+        public PresenterStatusStrip(IPresenterStatusStrip view)
+        {
+            View = view;
+        }
+    }
+}
