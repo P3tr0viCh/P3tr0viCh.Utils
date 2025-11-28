3d9bb4b340335a05242e8696acfa385814177b61
Константин Петрович <P3tr0viCh@users.noreply.github.com>
2025-11-28 15:08:26 +0500

    + WindowMessages.

--- P3tr0viCh.Utils/WindowMessages.cs
+++ P3tr0viCh.Utils/WindowMessages.cs
@@ -1,13 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Linq;
 
 namespace P3tr0viCh.Utils
 {
     public class WindowMessages
     {
+        public class ReceivedWindowMessageEventArgs : EventArgs
+        {
+            private readonly WindowMessage windowMessage;
+
+            public WindowMessage WindowMessage => windowMessage;
+
+            public ReceivedWindowMessageEventArgs(WindowMessage windowMessage)
+            {
+                this.windowMessage = windowMessage;
+            }
+        }
+
+        public delegate void ReceivedWindowMessageEventHandler(object sender, ReceivedWindowMessageEventArgs e);
+
         public class WindowMessage
         {
             public int Id { get; private set; }
             public string Message { get; private set; }
+
+            public event ReceivedWindowMessageEventHandler ReceivedWindowMessage;
 
             public WindowMessage(string message)
             {
@@ -24,6 +43,23 @@ namespace P3tr0viCh.Utils
             public bool PostMessage()
             {
                 return NativeMethods.PostMessage(NativeMethods.HWND_BROADCAST, Id, IntPtr.Zero, IntPtr.Zero);
+            }
+
+            internal void OnReceivedWindowMessage(ReceivedWindowMessageEventArgs e)
+            {
+                ReceivedWindowMessage?.Invoke(this, e);
+            }
+        }
+
+        public List<WindowMessage> Messages { get; set; } = new List<WindowMessage>();
+
+        public void WndProc(ref Message m)
+        {
+            var msgId = m.Msg;
+
+            foreach (var msg in Messages.Where(msg => msg.Id == msgId))
+            {
+                msg.OnReceivedWindowMessage(new ReceivedWindowMessageEventArgs(msg));
             }
         }
     }
