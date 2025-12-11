56e1f5acd2fd46940d7fdc993273be289bf5baad
Константин Петрович <P3tr0viCh@users.noreply.github.com>
2025-12-11 14:13:24 +0500

    ~ Login.

---
+++ P3tr0viCh.Utils/LocalizedAttributes.cs
@@ -0,0 +1,7 @@
+﻿namespace P3tr0viCh.Utils
+{
+    internal class LocalizedAttributes
+    {
+        public const string ResourceName = "Properties.Resources.P3tr0viCh.Utils";
+    }
+}
--- P3tr0viCh.Utils/Login.cs
+++ P3tr0viCh.Utils/Login.cs
@@ -5,17 +5,17 @@ using static P3tr0viCh.Utils.PasswordProperty;
 namespace P3tr0viCh.Utils
 {
     [TypeConverter(typeof(PropertySortedConverter))]
-    [LocalizedAttribute.DisplayName("Login.DisplayName", "Properties.Resources.Utils")]
+    [LocalizedAttribute.DisplayName("Login.DisplayName", LocalizedAttributes.ResourceName)]
     public class Login
     {
         [PropertyOrder(100)]
-        [LocalizedAttribute.DisplayName("Login.User.DisplayName", "Properties.Resources.Utils")] 
+        [LocalizedAttribute.DisplayName("Login.User.DisplayName", LocalizedAttributes.ResourceName)] 
         public string User { get; set; }
 
         [PropertyOrder(101)]
         [PasswordPropertyText(true)]
         [JsonConverter(typeof(PasswordConverter))]
-        [LocalizedAttribute.DisplayName("Login.Password.DisplayName", "Properties.Resources.Utils")]
+        [LocalizedAttribute.DisplayName("Login.Password.DisplayName", LocalizedAttributes.ResourceName)]
         public string Password { get; set; }
     }
 }
