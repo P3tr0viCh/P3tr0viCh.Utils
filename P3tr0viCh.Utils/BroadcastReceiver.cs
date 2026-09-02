32d8ce9d1eacccdb63043cc1859ebc162fa1094c
Константин Петрович <P3tr0viCh@users.noreply.github.com>
2026-09-02 09:56:00 +0500

    + BroadcastReceiver.

---
+++ P3tr0viCh.Utils/BroadcastReceiver.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace P3tr0viCh.Utils
+{
+    public class BroadcastReceiver : NativeWindow, IDisposable
+    {
+        private bool _disposed;
+
+        public BroadcastReceiver()
+        {
+            var cp = new CreateParams
+            {
+                Caption = "BroadcastReceiver",
+                ClassName = null,
+                Style = NativeMethods.WS_POPUP,
+                ExStyle = NativeMethods.WS_EX_NOACTIVATE,
+                Parent = IntPtr.Zero,
+                Width = 0,
+                Height = 0
+            };
+
+            CreateHandle(cp);
+        }
+
+        ~BroadcastReceiver() => Dispose();
+
+        public void Dispose()
+        {
+            if (_disposed) return;
+
+            DestroyHandle();
+
+            _disposed = true;
+        }
+
+        public delegate void ReceivedMessageEventHandler(object sender, ref Message m);
+
+        public event ReceivedMessageEventHandler ReceivedMessage;
+
+        internal void OnReceivedMessage(ref Message m)
+        {
+            ReceivedMessage?.Invoke(this, ref m);
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            OnReceivedMessage(ref m);
+
+            base.WndProc(ref m);
+        }
+    }
+}
--- P3tr0viCh.Utils/NativeMethods.cs
+++ P3tr0viCh.Utils/NativeMethods.cs
@@ -7,6 +7,9 @@ namespace P3tr0viCh.Utils
     {
         public static readonly IntPtr HWND_BROADCAST = new IntPtr(0xffff);
 
+        public static readonly int WS_POPUP = unchecked((int)0x80000000);
+        public static readonly int WS_EX_NOACTIVATE = unchecked(0x08000000);
+
         [DllImport("user32")]
         public static extern bool PostMessage(IntPtr hwnd, int msg, IntPtr wparam, IntPtr lparam);
 
